ca21dcb97cde043dcda70ec51229d48e949fae84
leesomin7747 <leesomin7747@gmail.com>
2026-07-23 02:59:58 +0900

    fix: 출결 타이틀 영문 오타 수정

--- src/routes/user/UserAttendancePage.tsx
+++ src/routes/user/UserAttendancePage.tsx
@@ -12,7 +12,7 @@ export function UserAttendancePage() {
         <div className="relative border-x border-secondary-2 bg-secondary-2/10 px-24 py-4">
           <span className="absolute -left-8 -top-8 h-16 w-16 rounded-full bg-secondary-2" />
           <span className="absolute -right-8 -bottom-8 h-16 w-16 rounded-full bg-secondary-2" />
-          <h1 className="text-h1 text-black">ATTENDENCE</h1>
+          <h1 className="text-h1 text-black">ATTENDANCE</h1>
         </div>
         <p className="text-sm-18 text-black">출석체크</p>
       </header>
